4b74073050f32582debba0909360fc57e207acec
mackswell13 <maxsherwin1323@gmail.com>
2025-03-06 07:50:04 -0600

    refactor blogs and auth generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,9 +16,6 @@ Rails.application.routes.draw do
   # Undestading routing
   root "blog_posts#index"
   # Routes are processed in order so /new needs to happen before /:id
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+
+  resources :blog_posts
 end
