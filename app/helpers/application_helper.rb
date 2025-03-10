908bf37d042279234324168b4fa96c26808d9325
Maxwell Sherwin <maxwellsherwin@proton.me>
2025-03-09 21:33:02 -0500

    added some md editing in blog body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def render_markdown(text)
+    options = {
+      autolink: true,
+      tables: true,
+      fenced_code_blocks: true,
+      strikethrough: true
+    }
+    renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer, options)
+    markdown.render(text).html_safe
+  end
 end
