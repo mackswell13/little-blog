89392a08b43c8b07418820b8b6c395462d9d7928
mackswell13 <maxsherwin1323@gmail.com>
2025-03-13 16:59:47 -0500

    Add resume entry models and controllers

--- app/controllers/resume_entries_controller.rb
+++ app/controllers/resume_entries_controller.rb
@@ -1,2 +1,8 @@
 class ResumeEntriesController < ApplicationController
+  allow_unauthenticated_access only: [ :index ]
+
+
+  def index
+    @entries = ResumeEntry.first(6)
+  end
 end
--- app/models/resume_entry.rb
+++ app/models/resume_entry.rb
@@ -1,2 +1,4 @@
 class ResumeEntry < ApplicationRecord
+  validates :title, presence: true
+  validates :bullets, presence: true
 end
