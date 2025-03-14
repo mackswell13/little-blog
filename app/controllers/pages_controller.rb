ab3b07253f92ad7bd5c4521dc52f8d85514d3d9d
mackswell13 <maxsherwin1323@gmail.com>
2025-03-13 21:54:18 -0500

    Add dates to resume form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   allow_unauthenticated_access
 
   def home
+    @blog_posts = BlogPost.first(6)
   end
 end
--- app/controllers/resume_entries_controller.rb
+++ app/controllers/resume_entries_controller.rb
@@ -1,8 +1,11 @@
 class ResumeEntriesController < ApplicationController
   allow_unauthenticated_access only: [ :index ]
 
-
   def index
     @entries = ResumeEntry.first(6)
+  end
+
+  def new
+    @resume_entry = ResumeEntry.new
   end
 end
